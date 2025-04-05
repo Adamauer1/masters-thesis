d861813de6053a8a2b636700d3b681e2cb72e46e
Adamauer1 <adamauer1@gmail.com>
2025-04-06 01:55:57 +0200

    adding simple category selection

--- script_gen.py
+++ script_gen.py
@@ -1,4 +1,8 @@
+import math
 import os
+
+import numpy as np
+import pandas as pd
 
 def generate_script(template_path, output_path, placeholders=None):
     if placeholders is None:
@@ -19,6 +23,59 @@ def generate_script(template_path, output_path, placeholders=None):
     print(f"File generated: {output_path}")
 
 
+
+# def load_and_label_points_np(file_path):
+#     data = np.genfromtxt(file_path, delimiter=',', names=True)
+#     sections = [get_section_label(x, y) for x, y in zip(data['x'], data['y'])]
+#     labeled_data = np.core.records.fromarrays(
+#         [data['x'], data['y'], sections],
+#         names='x, y, section'
+#     )
+#     return labeled_data
+
+arousal_file_path = "Data/deam/arousal.csv"
+arousal_df = pd.read_csv(arousal_file_path)
+valence_file_path = "Data/deam/valence.csv"
+valence_df = pd.read_csv(valence_file_path)
+
+# Display first few rows to confirm structure
+#print(arousal_df.head())
+
+test_v = valence_df.head(1).to_numpy()[0]
+test_a = arousal_df.head(1).to_numpy()[0]
+test_a = test_a[:-1]
+print(len(test_v))
+print(len(test_a))
+test = np.column_stack((test_v, test_a))
+print(test[1])
+
+angle = math.degrees(math.atan2(test[2][1], test[1][0]))
+
+cat = 0
+
+if angle < 0:
+    angle += 360
+
+    if 0 <= angle < 45:
+        cat = 2
+    elif 45 <= angle < 90:
+        cat = 1
+    elif 90 <= angle < 135:
+        cat = 8
+    elif 135 <= angle < 180:
+        cat = 7
+    elif 180 <= angle < 225:
+        cat = 6
+    elif 225 <= angle < 270:
+        cat = 5
+    elif 270 <= angle < 315:
+        cat = 4
+    else:  # 315 <= angle < 360
+        cat = 3
+
+print(angle)
+print(cat)
+
 template_file = "scriptTemplates/JulianTest.jwfscript"
 new_file = "scripts/TestScript.jwfscript"
 placeholders = {
@@ -26,4 +83,5 @@ placeholders = {
     "PLACEHOLDER_2": 2
 }
 
-generate_script(template_file, new_file, placeholders)
+#generate_script(template_file, new_file, placeholders)
+
